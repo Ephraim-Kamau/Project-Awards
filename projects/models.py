cac8300afa1076835db8764fb3ca78fa0843f3d4
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-24 14:43:39 +0300

    Add models to database

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.TextField(max_length=200)
+    description = models.TextField()
+    link_url = models.URLField(max_length=200)
+
+class Profile(models.Model):
+    bio = models.TextField(max_length=200)
+    contact = models.IntegerField
+    projects = models.ForeignKey(Projects)
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    
