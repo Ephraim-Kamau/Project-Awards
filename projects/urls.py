5998b7cf36facdc1617b299c59e8f04ef7350143
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-24 11:32:55 +0300

    Add dynamic contents for previous projects

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.projects_today,name='projectsToday')
+    url('^today/$',views.projects_today,name='projectsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_projects,name = 'pastProjects'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -19,6 +19,25 @@ def projects_today(request):
         </html>
             '''
     return HttpResponse(html)    
+
+def past_projects(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()    
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Projects for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)   
 
 def convert_dates(dates):
 
